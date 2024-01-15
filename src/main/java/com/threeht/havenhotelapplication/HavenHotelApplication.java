b9d0733ee407207513d2e8f5319e1c4ae3cf8f73
Hoang Vo <you@example.com>
2024-01-15 09:54:39 +0700

    Initial Commit

---
+++ src/main/java/com/threeht/havenhotelapplication/HavenHotelApplication.java
@@ -0,0 +1,13 @@
+package com.threeht.havenhotelapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HavenHotelApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HavenHotelApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/threeht/havenhotelapplication/HavenHotelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.threeht.havenhotelapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HavenHotelApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
