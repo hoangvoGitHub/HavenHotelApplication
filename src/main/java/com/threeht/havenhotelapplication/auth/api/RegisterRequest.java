e7d14ac5b8e0239a6bc8f2955c146cb820eb98d6
Hoang Vo <you@example.com>
2024-01-20 08:27:23 +0700

    Security

---
+++ src/main/java/com/threeht/havenhotelapplication/auth/api/RegisterRequest.java
@@ -0,0 +1,22 @@
+package com.threeht.havenhotelapplication.auth.api;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class RegisterRequest   {
+
+    private String username;
+
+    private String password;
+
+    private String firstName;
+
+    private String lastName;
+
+}
