ad9c7d161d05461593c0f50d3d4d5f734a6a09e7
Jackize <nguyenthanhhao2042002@Gmail.com>
2024-01-22 14:14:19 +0700

    Add curd room

--- src/main/java/com/threeht/havenhotelapplication/repository/RoomRepository.java
+++ src/main/java/com/threeht/havenhotelapplication/repository/RoomRepository.java
@@ -2,6 +2,11 @@ package com.threeht.havenhotelapplication.repository;
 
 import com.threeht.havenhotelapplication.model.Room;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface RoomRepository extends JpaRepository<Room, Long> {
+    @Query("SELECT r.roomType FROM Room r")
+    List<String> findDistinctRoomTypes();
 }
---
+++ src/main/java/com/threeht/havenhotelapplication/service/BookingSerivce.java
@@ -0,0 +1,13 @@
+package com.threeht.havenhotelapplication.service;
+
+import com.threeht.havenhotelapplication.model.BookedRoom;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class BookingSerivce {
+    public List<BookedRoom> getAllBookingsByRoomId(Long roomId) {
+        return  null;
+    }
+}
